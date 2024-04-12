bdd433432768ed2e2ee256a02f60d87b4c168072
njohan22 <159239609+njohan22@users.noreply.github.com>
2024-04-12 13:17:00 +0200

    Add files via upload

--- Security Market Line.R
+++ Security Market Line.R
@@ -11,39 +11,43 @@ returns <- read_csv("returns.csv")
 n = as.numeric(ncol(prices))
 
 #Calculate the expected return of S&P100
-sample_means = (1+apply(returns, 2, mean))^{252}-1
+sample_means = apply(returns, 2, mean)
 
 #Risk Free Asset
-rf = 0.0446
+rf = (1+0.0446)^{1/365}-1
 
 #Range of beta
 beta_theory = seq(0,2,0.01)
 
 #Import Market Portfolio
-market_portfolio <- read_csv("market_portfolio.csv")
+market_portfolio <- tq_get('^SP100',
+                    from = '2015-01-01',
+                    to = '2020-01-01',
+                    get = 'stock.prices')[,8]
 
-#Daily returns of the market portfolio
-market_daily = (as.matrix(returns)%*%as.matrix(market_portfolio[-1,2]))
+#Daily returns of the SP100
+
+market_daily <- na.omit(market_portfolio / lag(market_portfolio)) - 1
+
 
 #Calculates the expected return and variance of the market portfolio
-rm = mean(market_daily)
+rm = as.numeric(apply(market_daily, 2, mean))
 market_var = var(market_daily)[1,1]
 
-#Security market line function#
-sml = function(b) {
-  rf + beta*(as.numeric(rm) - as.numeric(rf))
-}
-
-#Calulates the covariance between asset i and the market portfolio
+#Calculates the covariance between asset i and the market portfolio
 covar = function(i) {
   cov(market_daily, returns[,i])
 }
 
 #Calculates the Beta of all assets
 beta = function(i) {
-  covar(i) /market_var
+  covar(i)/market_var
 }
 
+#Security market line function#
+sml = function(b) {
+  rf + beta_theory*(rm - rf)
+}
 
 #Plot
 x = c()
@@ -56,6 +60,16 @@ for (i in 1:n){
   y = c(y,sample_means[i])
 }
 
-plot(x,y)
-plot(sml)
+#Creates dataframes for plotting
+df_data <- data.frame(x,y,colnames(returns))
+df_sml <- data.frame(beta_theory = beta_theory, sml = sml(beta_theory))
+
+SML <- ggplot(df_data , aes(x = x, y = y, color = colnames(returns))) + 
+  geom_point(size = 2) + 
+  geom_line (data = df_sml , aes(x = beta_theory, y = sml), color = "black") +
+  geom_text(aes(label = colnames(returns)), vjust = -0.5, hjust = 1) +
+  theme(legend.position = "none") +
+  labs(x = "Beta", y = "Expected Daily Return")
+SML
+
 
