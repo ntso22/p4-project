5e6ab9698dbda6b10c5f63301bf57a30bbe30ad3
ntso22 <159235666+ntso22@users.noreply.github.com>
2024-04-23 12:43:18 +0200

    Update

--- experiment.r
+++ experiment.r
@@ -8,14 +8,13 @@ RETURNS <- data.frame(read_csv("returns_15_24.csv"))
 rf <- (1 + 0.0446)^(1/365) - 1
 test <- data.frame(read_csv("market_portfolio.csv"))[2]
 
-getSymbols("^GSPC", src="yahoo", from="2015-01-01", to="2024-01-01")
-sp100_index <- Ad(GSPC)[-c(1:1000, nrow(GSPC))] * 100 / as.numeric(Ad(GSPC)[1001])
-sp100_index
+MEMORY <- 500 # Determines the amount of trading days, the historic returns are calculated with
+
 # Reevaluate markowitz efficient portfolio each time step
 # Note that sufficient data berfore 'time' variable must exist. Check for that when calling function
 evaluate_markowitz_rf <- function(time, tau) {
     # Evaluating expectation and covariance for time step
-    scope <- RETURNS[(time - 1000):(time - 1), ]
+    scope <- RETURNS[(time - MEMORY):(time - 1), ]
     C <- cov(scope)
     r <- sapply(scope, FUN=mean)
     C_inv <- ginv(C)
@@ -34,24 +33,25 @@ evaluate_markowitz_rf <- function(time, tau) {
 # Compute return for active fund
 create_index <- function(tau, price_index) {
     price_history <- c(price_index)
-    for (t in 1001:nrow(RETURNS) - 1) {
-        if (t > 1000) {
+    for (t in (MEMORY + 1):nrow(RETURNS) - 1) {
+        if (t > MEMORY) {
             portfolio <- evaluate_markowitz_rf(t, tau)
             price_index <- price_index * (1 + as.numeric(as.matrix(RETURNS[t, ]) %*% as.vector(portfolio[-1])) + rf * portfolio[1])
-            price_history[t - 999] <- price_index
+            price_history[t - (MEMORY - 1)] <- price_index
         } else {
             next
         }
     }
-    return (price_history)
+    getSymbols("^GSPC", src="yahoo", from="2015-01-01", to="2024-01-01")
+    sp100_index <- Ad(GSPC)[-c(1:MEMORY, nrow(GSPC))] * 100 / as.numeric(Ad(GSPC)[MEMORY + 1])
+    df <- data.frame(Markowitz=price_history, SP100=sp100_index)
+    return (df)
 }
 
 # Plotting data
-index <- create_index(0.05, 100)
-matrix <- cbind(sp100_index, index)
-df <- data.frame(matrix)
+df <- create_index(0.05, 100)
 
-ggplot(data = df, aes(x=index(df$index), y=index)) + 
+ggplot(data = df, aes(x=index(df$Markowitz), y=Markowitz)) + 
     geom_line() + 
     geom_line(data = df, aes(x=index(df$GSPC.Adjusted), y=GSPC.Adjusted), color="red") +
     labs(x="days")
